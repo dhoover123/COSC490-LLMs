fd8846085663a76203b259228185650259fda23b
dhoover123 <144870622+dhoover123@users.noreply.github.com>
2025-10-28 14:44:43 -0400

    Delete 490projectCode/config2.py

--- 490projectCode/config2.py
+++
@@ -1,8 +0,0 @@
-# OpenRouter API Configuration AIzaSyDdbGo_Vz5SKBcUMS_qlQpAY5XVXEW9yO4
-# Model: google/gemma-3-4b-it:free, gemini-2.0-flash
-#https://openrouter.ai/api/v1
-#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
-OPENROUTER_API_KEY = " AIzaSyDdbGo_Vz5SKBcUMS_qlQpAY5XVXEW9yO4"
-OPENROUTER_BASE_URL = "https://generativelanguage.googleapis.com/v1beta/openai/"
-SITE_URL = "http://localhost:8000"  # Your site URL
-SITE_NAME = "D&D Game"  # Your site name 
